4e23f25ceb9e8225cd66865d3acb164dd4483ae6
Harrison Parkes <hpa101@uclive.ac.nz>
2022-10-05 15:33:50 +1300

    new board functions

--- assignment/Board.c
+++ assignment/Board.c
@@ -27,3 +27,31 @@ void board_set(Pos_t pos, Cell_State_t state)
 {
     board[pos.row][pos.col] = state;
 }
+
+bool is_on_board(Pos_t pos)
+{
+    if (pos.row > BOARD_HEIGHT - 1)
+        return false;
+
+    if ( pos.col > BOARD_WIDTH - 1)
+        return false;
+
+    return true;
+}
+
+Pos_t move_to_board(int8_t row, int8_t col)
+{
+    if (row < 0)
+        row = 0;
+    else if (row > BOARD_HEIGHT - 1)
+        row = BOARD_HEIGHT - 1;
+
+    if (col < 0)
+        col = 0;
+    else if(col > BOARD_WIDTH - 1)
+        col = BOARD_WIDTH - 1;
+
+    return (Pos_t) {
+        .row = row, .col = col
+    };
+}
--- assignment/Board.h
+++ assignment/Board.h
@@ -6,6 +6,7 @@
 #define ENCE260_ASSIGNMENT_BOARD_H
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "Position.h"
 
 #define BOARD_WIDTH 5
@@ -21,6 +22,8 @@ typedef enum cell_state_e {
 extern Cell_State_t board_get(Pos_t pos);
 extern void board_set(Pos_t pos, Cell_State_t state);
 
+extern bool is_on_board(Pos_t pos);
+extern Pos_t move_to_board(int8_t row, int8_t col);
 
 
 #endif //ENCE260_ASSIGNMENT_BOARD_H
