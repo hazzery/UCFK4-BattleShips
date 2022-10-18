ce722796cdee19c4a223e1350db5ea3113f67038
Harrison Parkes <hpa101@uclive.ac.nz>
2022-10-18 15:51:34 +1300

    Removed pacing IR comms

--- Communications.c
+++ Communications.c
@@ -63,21 +63,17 @@ void swap_board_data(Bitmap_t compressed_board)
 
     Bitmap_t oppositions_compressed_board;
 
-    // Slow down pacer for IR communication
-    pacer_init(TRANSFER_FREQUENCY);
     bool board_received = false;
 
     if (is_player_one) {
         // Player1 transfers their board data first...
         for (uint8_t col = 0; col < BOARD_WIDTH; col++) {
             ir_uart_putc(compressed_board[col]);
-            pacer_wait();
         }
         while (!board_received) {
             // ...then receives the opposition's.
             for (uint8_t col = 0; col < BOARD_WIDTH; col++) {
                 oppositions_compressed_board[col] = ir_uart_getc();
-                //pacer_wait();
                 led_set(BLUE_LED, HIGH);
             }
             board_received = true;
@@ -87,18 +83,14 @@ void swap_board_data(Bitmap_t compressed_board)
         while (!board_received) {
             for (uint8_t col = 0; col < BOARD_WIDTH; col++) {
                 oppositions_compressed_board[col] = ir_uart_getc();
-                pacer_wait();
             }
             board_received = true;
         }
         for (uint8_t col = 0; col < BOARD_WIDTH; col++) {
             ir_uart_putc(compressed_board[col]);
-            pacer_wait();
         }
     }
     led_set(BLUE_LED, LOW);
-    // Speed the pacer back up for running the display
-    pacer_init(DISPLAY_FREQUENCY);
     uncompress_board(oppositions_compressed_board, &oppositions_board);
 }
 
@@ -113,7 +105,6 @@ char wait_for_signal(bool is_player_one)
     char signal;
 
     while (!valid_signal) {
-        while (!ir_uart_read_ready_p()) continue;
         signal = ir_uart_getc();
         if ((signal == NEXT_TURN_SIGNAL_P1 && is_player_one) || (signal == NEXT_TURN_SIGNAL_P2 && !is_player_one) || signal == WIN_SIGNAL) {
             valid_signal = !valid_signal;
@@ -129,7 +120,6 @@ char wait_for_signal(bool is_player_one)
  */
 void win_signal(void)
 {
-    while (!ir_uart_write_ready_p()) continue;
     ir_uart_putc(WIN_SIGNAL);
 
     ghostGL_show_bitmap(win_bitmap);
